2798d9ccd36d7f1453cc74473b31d48f245ae866
Jan Prochazka <jan.prochazka@gmail.com>
2019-11-24 17:03:01 +0100

    build script

--- examples/js/database.sql
+++ examples/js/database.sql
@@ -109,3 +109,70 @@ PRINT 'Creating index IX_FK_Album_ArtistId on Album'
 CREATE  INDEX [IX_FK_Album_ArtistId] ON [Album] ([ArtistId]) 
 END
 GO
+DECLARE @sql NVARCHAR(MAX) = '';
+IF (NOT EXISTS(
+      SELECT * FROM INFORMATION_SCHEMA.VIEWS WHERE TABLE_NAME='AlbumView'
+      ))
+BEGIN
+PRINT 'Creating view AlbumView'
+SET @sql = 'CREATE VIEW';
+END
+ELSE
+BEGIN
+PRINT 'Altering view AlbumView'
+SET @sql = 'ALTER VIEW';
+END
+SET @sql = @sql + ' ' + ' AlbumView 
+as
+select 
+  Album.Title as Album, Artist.Name as Artist 
+from Album
+left join Artist on Album.ArtistId = Artist.ArtistId
+';
+EXEC sp_executesql @sql;
+GO
+DECLARE @sql NVARCHAR(MAX) = '';
+IF (NOT EXISTS(
+      SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE ROUTINE_NAME='UNSAFE_DropAllTables'
+      ))
+BEGIN
+PRINT 'Creating procedure UNSAFE_DropAllTables'
+SET @sql = 'CREATE PROCEDURE';
+END
+ELSE
+BEGIN
+PRINT 'Altering procedure UNSAFE_DropAllTables'
+SET @sql = 'ALTER PROCEDURE';
+END
+SET @sql = @sql + ' ' + ' UNSAFE_DropAllTables(@confirm VARCHAR(50))
+AS
+BEGIN
+  IF (@confirm <> ''YES'') RETURN;
+
+  DECLARE @name VARCHAR(128)
+  DECLARE @table VARCHAR(128)
+  DECLARE @constraint VARCHAR(254)
+  DECLARE @SQL VARCHAR(254)
+
+  SELECT TOP 1 @table=TABLE_NAME, @constraint=CONSTRAINT_NAME FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE constraint_catalog=DB_NAME() AND CONSTRAINT_TYPE = ''FOREIGN KEY'' ORDER BY TABLE_NAME
+
+  WHILE @table IS NOT NULL AND @constraint IS NOT NULL
+  BEGIN
+      SELECT @SQL = ''ALTER TABLE [dbo].['' + RTRIM(@table) +''] DROP CONSTRAINT ['' + RTRIM(@constraint)+'']''
+      EXEC (@SQL)
+      PRINT ''Dropped FK Constraint: '' + @constraint + '' on '' + @table
+      SELECT TOP 1 @table=TABLE_NAME, @constraint=CONSTRAINT_NAME FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE constraint_catalog=DB_NAME() AND CONSTRAINT_TYPE = ''FOREIGN KEY'' ORDER BY TABLE_NAME
+  END
+
+  SELECT @name = (SELECT TOP 1 [name] FROM sysobjects WHERE [type] = ''U'' AND category = 0 ORDER BY [name])
+  WHILE @name IS NOT NULL
+  BEGIN
+      SELECT @SQL = ''DROP TABLE [dbo].['' + RTRIM(@name) +'']''
+      EXEC (@SQL)
+      PRINT ''Dropped Table: '' + @name
+      SELECT @name = (SELECT TOP 1 [name] FROM sysobjects WHERE [type] = ''U'' AND category = 0 AND [name] > @name ORDER BY [name])
+  END
+END 
+';
+EXEC sp_executesql @sql;
+GO
